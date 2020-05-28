bafc3a2bbe8c9a011e0bbcef0e08144fdc937cb3
Moksh <moksh@pop-os.localdomain>
2020-05-28 16:30:12 +0530

    Added Delete task and Changed enterypoints

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
+var ObjectId = require('mongodb').ObjectId;
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
-app.get('/test',function(req,res){
+//======================================================
+// Get The Tasks
+app.get('/getTask',function(req,res){
    MongoClient.connect(url, function(err, db) {
       if (err) throw err;
       var dbo = db.db("mydb");
@@ -13,14 +16,12 @@ app.get('/test',function(req,res){
          res.json(result);
        });
      });
-     
    });
 
-var server = app.listen(8081, function () {
-   console.log("Example app listening at 8081");
-});
-
-app.get('/todo', async function(req, res) {
+//========================================================
+//Insert TO_DO
+app.get('/insertTask', async function(req, res) {
+   // Add other Parameters here
    let title = req.query.title;
    let status = req.query.status;
    let createdby = req.query.createdby;
@@ -31,7 +32,7 @@ app.get('/todo', async function(req, res) {
    console.log(title,status,createdby);
    MongoClient.connect(url, function(err, db) {
       if (err) throw err;
-      var dbo = db.db("mydb");
+      var dbo = db.db("mydb"); //DB
       var myobj = {"title": title, "status":status,"CreatedBy":createdby,"time":Date.now()};
       dbo.collection("tasks").insertOne(myobj, function(err, res) {
         if (err) throw err;
@@ -40,6 +41,31 @@ app.get('/todo', async function(req, res) {
       });
       res.end("Inserted:"+title+" "+status);
    });
-   
    }
+});
+
+//========================================================
+// Delete Task
+app.get('/deleteTask', async function(req, res) {
+   if(!req.query.id){
+      res.end("Wrong ID");
+   }
+   else{
+   MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("mydb");
+      var myquery = {"_id":ObjectId(req.query.id)};
+      dbo.collection("tasks").deleteOne(myquery, function(err, obj) {
+        if (err) throw err;
+        console.log(dbo.collection('tasks').findOne(req.query.url));
+        res.end("1 document deleted");
+        db.close();
+      });
+   });}
+});
+
+//========================================================
+// Serve 
+var server = app.listen(8081, function () {
+   console.log("Example app listening at 8081");
 });
