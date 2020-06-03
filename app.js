0d21bc933a5507b7d66baca47367044143eebbd9
makrandr1999 <makrand.r@somaiya.edu>
2020-06-03 18:23:26 +0530

    Add basic error handling

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const userRoutes = require('./api/routes/users');
+
+app.use(morgan('dev'));
 app.use('/users',userRoutes);
+// app.use((req, res, next) => {
+//     res.status(200).json({
+//         message: 'Home Page',
+//     });
+// });
+
 app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'Home Page',
+    const error = new Error('Not Found');
+    error.status= 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message : error.message
+        }
     });
 });
 
