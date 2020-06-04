2860eab2e84841960a9c010ffa89e8bc631ed546
therexone <renerjake@gmail.com>
2020-06-04 11:34:38 +0530

    Initial Commit

---
+++ frontend/README.md
@@ -0,0 +1,6 @@
+# Kanban App frontend
+
+In the project directory do
+-`yarn install` then
+
+-`yarn start`
---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      App page
+    </div>
+  );
+}
+
+export default App;
