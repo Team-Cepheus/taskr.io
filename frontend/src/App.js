e1316ad1d7139809070e037ab097e46d0dd07e8e
therexone <renerjake@gmail.com>
2020-06-04 11:51:48 +0530

    index.html cleanup and Add: react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div>
-      App page
-    </div>
+    <BrowserRouter>
+      <Switch>
+        
+      </Switch>
+    </BrowserRouter>
   );
 }
 
