d7ef59eeaa2fd440e6535bb98804af4211b87085
Moksh <moksh@pop-os.localdomain>
2020-06-07 12:34:53 +0530

    Added addUser to workspace

--- api/routes/workspace.js
+++ api/routes/workspace.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const workspace = require("../models/workspace");
-const mongoose = require('mongoose')
-const Task = require('../models/tasks')
+const mongoose = require("mongoose");
+const Task = require("../models/tasks");
+const User = require("../models/users");
 
 router
   .route("/")
@@ -10,8 +11,8 @@ router
   .get((req, res) => {
     workspace
       .find()
-      .populate('tasks')
-      .populate('users', ['name', 'username', 'email'])
+      .populate("tasks")
+      .populate("users", ["name", "username", "email"])
       .exec()
       .then((doc) => {
         res.status(200).json(doc);
@@ -27,70 +28,88 @@ router
       name: req.body.name,
       admin: req.body.admin,
       users: req.body.users,
-      tasks: req.body.tasks
+      tasks: req.body.tasks,
     });
     workSpace
       .save()
-      .then(result => {
+      .then((result) => {
         console.log(result);
         res.status(201).json(result);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
         res.status(500).json({
-          error: err
+          error: err,
         });
       });
   });
 
 router
-  .route('/:workspaceID')
+  .route("/:workspaceID")
 
   .get((req, res) => {
-    workspace.findById(req.params.workspaceID).exec().then(result => {
-      res.status(200).json(result);
-    })
-      .catch(err => {
-        res.status(500).json(err);
+    workspace
+      .findById(req.params.workspaceID)
+      .exec()
+      .then((result) => {
+        res.status(200).json(result);
       })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
   })
 
   .delete((req, res) => {
     Workspace.remove({ _id: req.params.body })
       .exec()
-      .then(result => {
+      .then((result) => {
         res.status(200).json(result);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
         res.status(500).json({
-          error: err
+          error: err,
         });
       });
-  })
+  });
 
-router
-  .route('/:workspaceID/addTask/:taskID')
-  .patch(async (req, res) => {
-    var wexist = await workspace.exists({ _id: req.params.workspaceID })
-    var texist = await Task.exists({_id: req.params.taskID})
-    
-    if(wexist && texist){
-      const w = await workspace.findById(req.params.workspaceID).exec();
-      w.tasks.push(req.params.taskID);
-      w.save().then(result=>{
-        res.status(200).json(result)
+router.route("/:workspaceID/addTask/:taskID").patch(async (req, res) => {
+  var wexist = await workspace.exists({ _id: req.params.workspaceID });
+  var texist = await Task.exists({ _id: req.params.taskID });
+
+  if (wexist && texist) {
+    const w = await workspace.findById(req.params.workspaceID).exec();
+    w.tasks.push(req.params.taskID);
+    w.save()
+      .then((result) => {
+        res.status(200).json(result);
       })
-      .catch(err=>{
-        res.status(500).json(err)
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+router.route("/:workshopID/addUser/:UserID")
+.patch(async (req, res) => {
+  var wexist = await workspace.exists({ _id: req.params.workspaceID });
+  var uexist = await User.exists({ _id: req.params.UserID });
+
+  if (wexist && uexist) {
+    const w = await workspace.findById(req.params.workspaceID).exec();
+    w.users.push(req.params.UserID);
+    w.save()
+      .then((result) => {
+        res.status(200).json(result);
       })
-    }
-    else{
-      res.sendStatus(400);
-    }
-  })
-
-
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  } else {
+    res.sendStatus(400);
+  }
+});
 
 module.exports = router;
-
