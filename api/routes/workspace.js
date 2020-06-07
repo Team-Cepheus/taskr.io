585d871a211f8ab61cb7a8b3e627fb98521cc04e
Moksh <moksh@pop-os.localdomain>
2020-06-07 13:29:35 +0530

    changes to endpoint

--- api/routes/workspace.js
+++ api/routes/workspace.js
@@ -59,57 +59,78 @@ router
       });
   })
 
-  .delete((req, res) => {
-    Workspace.remove({ _id: req.params.body })
-      .exec()
-      .then((result) => {
-        res.status(200).json(result);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json({
-          error: err,
+
+router.route("/workspaceTask")
+  .post(async (req, res) => {
+    var wexist = await workspace.exists({ _id: req.body.workspaceID });
+    var texist = await Task.exists({ _id: req.body.taskID });
+
+    if (wexist && texist) {
+      const w = await workspace.findById(req.body.workspaceID).exec();
+      w.tasks.push(req.body.taskID);
+      w.save()
+        .then((result) => {
+          res.status(200).json(result);
+        })
+        .catch((err) => {
+          res.status(500).json(err);
         });
-      });
+    } else {
+      res.sendStatus(400);
+    }
+  })
+  .delete(async (req, res) => {
+    var wexist = await workspace.exists({ _id: req.body.workspaceID });
+    if (wexist) {
+      const w = await workspace.findById(req.body.workspaceID).exec();
+      w.tasks.pull({ _id: req.body.taskID })
+      w.save()
+        .then((result) => {
+          res.status(200).json(result);
+        })
+        .catch((err) => {
+          res.status(500).json(err);
+        });
+    } else {
+      res.sendStatus(400);
+    }
+  })
+
+router.route("/workspaceUser")
+  .post(async (req, res) => {
+    var wexist = await workspace.exists({ _id: req.body.workspaceID });
+    var uexist = await User.exists({ _id: req.body.userID });
+    console.log(wexist, uexist)
+    if (wexist && uexist) {
+      const w = await workspace.findById(req.body.workspaceID).exec();
+      w.users.push(req.body.userID);
+      w.save()
+        .then((result) => {
+          res.status(200).json(result);
+        })
+        .catch((err) => {
+          res.status(500).json(err);
+        });
+    } else {
+      res.sendStatus(400);
+    }
+  })
+  .delete(async (req, res) => {
+    var wexist = await workspace.exists({ _id: req.body.workspaceID });
+    if (wexist) {
+      const w = await workspace.findById(req.body.workspaceID).exec();
+      w.users.pull({ _id: req.body.userID })
+      w.save()
+        .then((result) => {
+          res.status(200).json(result);
+        })
+        .catch((err) => {
+          res.status(500).json(err);
+        });
+    } else {
+      res.sendStatus(400);
+    }
   });
 
-router.route("/:workspaceID/addTask/:taskID").patch(async (req, res) => {
-  var wexist = await workspace.exists({ _id: req.params.workspaceID });
-  var texist = await Task.exists({ _id: req.params.taskID });
-
-  if (wexist && texist) {
-    const w = await workspace.findById(req.params.workspaceID).exec();
-    w.tasks.push(req.params.taskID);
-    w.save()
-      .then((result) => {
-        res.status(200).json(result);
-      })
-      .catch((err) => {
-        res.status(500).json(err);
-      });
-  } else {
-    res.sendStatus(400);
-  }
-});
-
-router.route("/:workspaceID/addUser/:UserID")
-.patch(async (req, res) => {
-  var wexist = await workspace.exists({ _id: req.params.workspaceID });
-  var uexist = await User.exists({ _id: req.params.UserID });
-  console.log(wexist,uexist)
-  if (wexist && uexist) {
-    const w = await workspace.findById(req.params.workspaceID).exec();
-    w.users.push(req.params.UserID);
-    w.save()
-      .then((result) => {
-        res.status(200).json(result);
-      })
-      .catch((err) => {
-        res.status(500).json(err);
-      });
-  } else {
-    res.sendStatus(400);
-  }
-});
 
 module.exports = router;
