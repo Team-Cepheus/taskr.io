6b95499d3ae87e313e2966d2723692fbe35e4ff1
therexone <renerjake@gmail.com>
2020-06-09 07:54:17 +0530

    add client side login validation

--- frontend/src/components/Login/LoginForm.jsx
+++ frontend/src/components/Login/LoginForm.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const LoginForm = () => {
+
+    const [ username , setUsername ] = useState('')
+    const [ password, setPassword ] = useState('')
+    const [error, setError ] = useState(null)
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if(!password || ! username){
+            setError('Please enter username and password')
+        }
+
+    }
+
     return (
-        <form action="">
-            <input type="text" placeholder="Username" />
-            <input type="password" name="" placeholder="Password" />
+        <form onSubmit={onSubmit}>
+            <input type="text" placeholder="Username" name="username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+            <input type="password" name="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+            { error && <p className="error-text">{error}</p>}
             <button className="btn" type="submit">LOGIN</button>
         </form>
     );
--- frontend/src/components/Login/LoginPage.jsx
+++ frontend/src/components/Login/LoginPage.jsx
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../styles/LoginPage.css';
 import LoginForm from './LoginForm';
 import SignupForm from './SignupForm';
 
 const LoginPage = () => {
+
+    const [login, setLogin] = useState(true)
+
+
     return (
         <div className="login-page">
             <div className="login-form">
                 <h1 className="brand-title">taskr. <span>io</span> </h1>
                 <small className="brand-title--subtitle">A minimal kanban task board</small>
                 <div className="login-buttons">
-                    <button> LOGIN</button>
-                    <button>SIGN UP</button>
+                    <button className={`${login && 'active' }`} onClick={() => setLogin(true)}> LOGIN</button>
+                    <button className={`${!login && 'active' }`} onClick={() => setLogin(false)}>SIGN UP</button>
                 </div>
-                {/* <LoginForm/> */}
-                <SignupForm/>
-                {/* <a href="#">Forgot Password?</a> */}
+                {
+                    login ? <LoginForm /> : <SignupForm />
+                }
+
             </div>
         </div>
     );
